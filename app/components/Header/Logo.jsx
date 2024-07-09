152171044da34246d3917a16909c5464194ee963
mss-souloftware <sufyan.souloftware@gmail.com>
2024-07-09 18:24:37 +0500

    logo styling

--- app/components/Header/Logo.jsx
+++ app/components/Header/Logo.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Logo() {
     return (
-        <Link href='/'>
+        <Link href='/' className="p-2">
             <Image src="/Images/Global/logo.png" width={280} height={85} alt="Propvator" />
         </Link>
     )
