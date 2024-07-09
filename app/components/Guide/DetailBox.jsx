b446907a58c8d5d288b05770763a8bbd949408b8
mss-souloftware <sufyan.souloftware@gmail.com>
2024-07-09 15:49:48 +0500

    Guid boxes

---
+++ app/components/Guide/DetailBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './DetailBox.module.css'
+
+export default function DetailBox() {
+    return (
+        <div className={styles.DetailBox}>
+            <div className={styles.BoxIcon}>
+                <Image src='/Images/Global/comparison-icon.svg' width={70} height={70} alt='Exclusive Comparison' />
+            </div>
+            <h3>Exclusive Comparison</h3>
+            <p>
+                Compare the biggest prop firms in one place - no need to scour the internet for bits and pieces of information.Compare the biggest prop firms in one place - no need to scour the internet for bits and pieces of information.Compare the biggest prop firms in one place.
+            </p>
+        </div>
+    )
+}
--- app/components/Guide/Guide.jsx
+++ app/components/Guide/Guide.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import styles from './Guide.module.css'
+import DetailBox from './DetailBox';
 
 function Guide() {
   return (
     <div className="container mx-auto">
-        <h3 className={styles.Guideheading}>
+      <h3 className={styles.Guideheading}>
         The Ultimate Guide to Prop Trading Firms: <br /> Your One-Stop Hub for Success
-        </h3>
-
-
-        <div className="flex pt-20">
-           <div className="w-12/4">
-            <div className="blockbox">
-                <h6>Exclusive Comparison</h6>
-                <p>Compare the biggest prop firms in one place - no need to scour the internet for bits and pieces of information.Compare the biggest prop firms in one place - no need to scour the internet for bits and pieces of information.Compare the biggest prop firms in one place.</p>
-            </div>
-           </div>
-        </div>
+      </h3>
+      <div className="flex flex-wrap justify-between mt-14">
+        <DetailBox />
+        <DetailBox />
+        <DetailBox />
+        <DetailBox />
+        <DetailBox />
+        <DetailBox />
+        <DetailBox />
+        <DetailBox />
+        <DetailBox />
+      </div>
     </div>
   )
 }
