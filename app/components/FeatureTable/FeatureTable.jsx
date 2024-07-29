a00a6c8731d957e878fc4fed9b4be6c04583ced0
mss-souloftware <sufyan.souloftware@gmail.com>
2024-07-29 17:16:51 +0500

    pagination

--- app/components/FeatureTable/FeatureTable.jsx
+++ app/components/FeatureTable/FeatureTable.jsx
@@ -15,72 +15,14 @@ export default function FeatureTable({ filter }) {
     useEffect(() => {
         const filterData = () => {
             if (filter.hasTempered) {
-                // let filteredData = tableBodyData.filter(item => {
-                //   return Object.keys(filter.assetType).every(key => {
-                //     return filter.assetType[key] === false || item.filterType.assetType[key] === filter.assetType[key];
-                //   });
-                // });
-
-
                 let filteredData = tableBodyData.filter(item => {
                     for (const brand in filter.brands) {
-                        console.log(item.firm.toLowerCase(), brand, filter.brands[brand])
                         if (filter.brands[brand] && item.firm.toLowerCase() === brand) {
-                            console.log("saying done", filter.brands[brand] && item.firm.toLowerCase() === brand)
                             return true;
                         }
                     }
                     return false;
                 });
-
-                console.log("filteredData", filteredData)
-
-
-                // console.log("filteredData", filteredData)
-
-                // // Apply sizeType filter
-                // filteredData = tableBodyData.filter(item => {
-                //     return Object.keys(filter.sizeType).every(key => {
-                //         return filter.sizeType[key] === false || item.sizeType[key] === filter.sizeType[key];
-                //     });
-                // });
-
-                // // Apply accountTypes filter
-                // filteredData = tableBodyData.filter(item => {
-                //     return Object.keys(filter.accountTypes).every(key => {
-                //         return filter.accountTypes[key] === false || item.accountTypes[key] === filter.accountTypes[key];
-                //     });
-                // });
-
-                // // Apply countries filter
-                // filteredData = tableBodyData.filter(item => {
-                //     return Object.keys(filter.countries).every(key => {
-                //         return filter.countries[key] === false || item.countries[key] === filter.countries[key];
-                //     });
-                // });
-
-                // // Apply platforms filter
-                // filteredData = tableBodyData.filter(item => {
-                //     return Object.keys(filter.platforms).every(key => {
-                //         return filter.platforms[key] === false || item.platforms[key] === filter.platforms[key];
-                //     });
-                // });
-
-                // // Apply broker filter
-                // filteredData = tableBodyData.filter(item => {
-                //     return Object.keys(filter.broker).every(key => {
-                //         return filter.broker[key] === false || item.broker[key] === filter.broker[key];
-                //     });
-                // });
-
-                // // Apply assetType filter
-                // filteredData = tableBodyData.filter(item => {
-                //     return Object.keys(filter.assetType).every(key => {
-                //         return filter.assetType[key] === false || item.assetType[key] === filter.assetType[key];
-                //     });
-                // });
-
-
 
                 setData(filteredData);
             } else {
@@ -101,9 +43,15 @@ export default function FeatureTable({ filter }) {
         setCurrentPage(prevPage => prevPage - 1);
     };
 
+    const handlePageClick = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
     const indexOfLastListing = currentPage * listingsPerPage;
     const indexOfFirstListing = indexOfLastListing - listingsPerPage;
     const currentListings = data.slice(indexOfFirstListing, indexOfLastListing);
+
+    const totalPages = Math.ceil(data.length / listingsPerPage);
 
     if (loading) {
         return <div>Loading...</div>;
@@ -145,9 +93,6 @@ export default function FeatureTable({ filter }) {
                                                     <div className="company">
                                                         <Image className='mx-auto' width={20} height={20} src={item.logo} alt={item.firm} />
                                                         <p>{item.firm}
-                                                            {item.verified ?
-                                                                <svg fill="#0092F6" width="15px" height="15px" viewBox="0 0 512 512" id="_x30_1" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M434.068,46.758L314.607,9.034C295.648,3.047,275.883,0,256,0s-39.648,3.047-58.607,9.034L77.932,46.758  C52.97,54.641,36,77.796,36,103.973v207.39c0,38.129,18.12,73.989,48.816,96.607l117.032,86.234  C217.537,505.764,236.513,512,256,512s38.463-6.236,54.152-17.796l117.032-86.234C457.88,385.352,476,349.492,476,311.363v-207.39  C476,77.796,459.03,54.641,434.068,46.758z M347.924,227.716l-98.995,98.995c-11.716,11.716-30.711,11.716-42.426,0l-42.427-42.426  c-11.716-11.716-11.716-30.711,0-42.426l0,0c11.716-11.716,30.711-11.716,42.426,0l21.213,21.213l77.782-77.782  c11.716-11.716,30.711-11.716,42.426,0h0C359.64,197.005,359.64,216,347.924,227.716z" /></svg>
-                                                                : ''}
                                                         </p>
                                                     </div>
                                                 </td>
@@ -156,17 +101,6 @@ export default function FeatureTable({ filter }) {
                                                         <del>{item.salePrice}</del>
                                                         : ''}
                                                     <p>{item.price}</p>
-                                                    {/* <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M17 7.59302V13.407C17 14.359 16.5047 15.243 15.7039 15.7275L10.8002 18.643C9.99946 19.119 9.0088 19.119 8.19977 18.643L3.2961 15.7275C2.49533 15.2515 2 14.3675 2 13.407V7.59302C2 6.64102 2.49533 5.75699 3.2961 5.27249L8.19977 2.357C9.00054 1.881 9.9912 1.881 10.8002 2.357L15.7039 5.27249C16.5047 5.75699 17 6.63252 17 7.59302Z" fill="url(#paint0_linear_2051_8984)" />
-                                                        <path d="M10 14V14.0833" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                                        <path d="M10 6V10" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                                        <defs>
-                                                            <linearGradient id="paint0_linear_2051_8984" x1="2" y1="2.67928" x2="19.1109" y2="16.6949" gradientUnits="userSpaceOnUse">
-                                                                <stop offset="0.156958" stop-color="#7DE3FA" />
-                                                                <stop offset="0.741638" stop-color="#333BFF" />
-                                                            </linearGradient>
-                                                        </defs>
-                                                    </svg> */}
                                                 </td>
                                                 <td>
                                                     <p>{item.discount}</p>
@@ -203,9 +137,6 @@ export default function FeatureTable({ filter }) {
                                                 </td>
                                                 <td>
                                                     <Link href={`${item.buy}`} target='_blank' className="cart">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                            <path d="M2 3H3.35798C3.85767 3 4.29368 3.343 4.42301 3.835L4.79827 5.272M4.79827 5.272C10.2563 5.11589 15.7091 5.73515 21 7.112C20.1927 9.566 19.2334 11.95 18.1351 14.25H7.14387M4.79827 5.272L7.14387 14.25M7.14387 14.25C6.36431 14.25 5.61667 14.5661 5.06543 15.1287C4.5142 15.6913 4.20452 16.4544 4.20452 17.25H19.6361M5.6742 20.25C5.6742 20.4489 5.59678 20.6397 5.45897 20.7803C5.32116 20.921 5.13425 21 4.93936 21C4.74446 21 4.55756 20.921 4.41975 20.7803C4.28194 20.6397 4.20452 20.4489 4.20452 20.25C4.20452 20.0511 4.28194 19.8603 4.41975 19.7197C4.55756 19.579 4.74446 19.5 4.93936 19.5C5.13425 19.5 5.32116 19.579 5.45897 19.7197C5.59678 19.8603 5.6742 20.0511 5.6742 20.25ZM18.1665 20.25C18.1665 20.4489 18.089 20.6397 17.9512 20.7803C17.8134 20.921 17.6265 21 17.4316 21C17.2367 21 17.0498 20.921 16.912 20.7803C16.7742 20.6397 16.6968 20.4489 16.6968 20.25C16.6968 20.0511 16.7742 19.8603 16.912 19.7197C17.0498 19.579 17.2367 19.5 17.4316 19.5C17.6265 19.5 17.8134 19.579 17.9512 19.7197C18.089 19.8603 18.1665 20.0511 18.1665 20.25Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                        </svg>
                                                     </Link>
                                                 </td>
                                             </tr>
@@ -237,6 +168,19 @@ export default function FeatureTable({ filter }) {
                             >
                                 Next
                             </button>
+                        </div>
+                        <div className='pageCount relative'>
+                            <ul>
+                                {[...Array(totalPages)].map((_, i) => (
+                                    <li
+                                        key={i}
+                                        className={currentPage === i + 1 ? 'current' : ''}
+                                        onClick={() => handlePageClick(i + 1)}
+                                    >
+                                        {i + 1}
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     </div>
                 </div>
