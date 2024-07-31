f2748e571946f9aaa57a4d2b4d9f1406d3366037
mss-souloftware <sufyan.souloftware@gmail.com>
2024-07-31 18:25:47 +0500

    ranking table

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    // trailingSlash: true,
-    // skipTrailingSlashRedirect: false,
-    // distDir: 'dist'
+    output: 'export',
+    trailingSlash: true,
+    skipTrailingSlashRedirect: false,
+    distDir: 'dist'
 };
 
 export default nextConfig;
