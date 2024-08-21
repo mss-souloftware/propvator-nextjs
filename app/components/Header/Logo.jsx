3a073acf79bd31e2b28ede24ad69202f0ad9056e
mss-souloftware <sufyan.souloftware@gmail.com>
2024-08-21 11:58:20 +0500

    website live

--- app/components/Header/Logo.jsx
+++ app/components/Header/Logo.jsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function Logo({ demo }) {
     return (
         <Link href='/' className="p-2 d-inline-block relative">
-            {demo ?
+            {/* {demo ?
                 <span className="absolute bg-warning text-white px-2 rounded -right-14 text-sm">DEMO</span>
-                : ''}
+                : ''} */}
             <Image className="mx-auto" src="/Images/Global/logo.svg" width={280} height={85} alt="Propvator" />
         </Link>
     )
