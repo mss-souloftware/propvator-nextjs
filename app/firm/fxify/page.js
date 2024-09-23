0e0acd47d16d0e38e40be9030fffcdb6c102e932
mss-souloftware <sufyan.souloftware@gmail.com>
2024-09-23 12:40:08 +0500

    Fxify page

---
+++ app/firm/fxify/page.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import FirmHero from "../../components/Firms/FirmHero";
+import FirmContent from "../../components/Firms/FirmContent";
+import styles from "./../FirmHero.module.css";
+import Tabs from "../../components/Firms/Tabs/Tabs";
+
+export default function firm() {
+  return (
+    <>
+      <Header />
+      <div className={`${styles.heroSection}`}>
+        <FirmHero />
+        <Tabs />
+      </div>
+      <FirmContent />
+      <Footer />
+    </>
+  );
+}
